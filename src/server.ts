0a8ab9344c7f476aa9ff17b0568cb5b56ab61379
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-18 07:07:56 +0900

    feat: 영화 sql 틀 제작 (prisma 학습 준비)

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,46 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+    type Movie {
+        title: String
+        year: Int
+    }
+    type Query {
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
+    }
 `;
+interface MovieState {
+    title: string;
+    year: number;
+}
 
 const resolvers = {
-  Query: {
-    hello: () => "baby",
-  },
+    Query: {
+        movies: () => [],
+        movie: () => ({ title: "hello", year: 2022 }),
+    },
+    Mutation: {
+        createMovie: (_: any, { title }: MovieState) => {
+            console.log(title);
+            return true;
+        },
+        deleteMovie: (_: any, { title }: MovieState) => {
+            console.log(title);
+            return true;
+        },
+    },
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
 
 server
-  .listen(4000)
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+    .listen(4000)
+    .then(() => console.log("Server is running on http://localhost:4000/"));
