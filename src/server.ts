59815c57bd65ade563e7c1be246258d7f5ba83c5
Eum Young Hyun <opwer032@naver.com>
2022-01-18 23:36:42 +0900

    feat: prisma 첫 orm 생성

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,48 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-    type Movie {
-        title: String
-        year: Int
-    }
-    type Query {
-        movies: [Movie]
-        movie: Movie
-    }
-    type Mutation {
-        createMovie(title: String!): Boolean
-        deleteMovie(title: String!): Boolean
-    }
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
 `;
 interface MovieState {
-    title: string;
-    year: number;
+  id: number;
+  title: string;
+  year: number;
 }
 
 const resolvers = {
-    Query: {
-        movies: () => [],
-        movie: () => ({ title: "hello", year: 2022 }),
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "hello", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_: any, { title }: MovieState) => {
+      console.log(title);
+      return true;
     },
-    Mutation: {
-        createMovie: (_: any, { title }: MovieState) => {
-            console.log(title);
-            return true;
-        },
-        deleteMovie: (_: any, { title }: MovieState) => {
-            console.log(title);
-            return true;
-        },
+    deleteMovie: (_: any, { title }: MovieState) => {
+      console.log(title);
+      return true;
     },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 server
-    .listen(4000)
-    .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(4000)
+  .then(() => console.log("Server is running on http://localhost:4000/"));
