0bba4e0c27bdae6a225969fc0aa4e9363adf180b
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-19 07:26:31 +0900

    feat: 영화 디테일, 삭제 추가

--- src/server.ts
+++ src/server.ts
@@ -4,51 +4,51 @@ import { ApolloServer, gql } from "apollo-server";
 const client = new PrismaClient();
 
 const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Boolean
-  }
+    type Movie {
+        id: Int!
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
+    }
+    type Query {
+        movies: [Movie]
+        movie(id: Int!): Movie
+    }
+    type Mutation {
+        createMovie(title: String!, year: Int!, genre: String): Movie
+        deleteMovie(id: Int!): Movie
+    }
 `;
 interface MovieState {
-  id: number;
-  title: string;
-  year: number;
-  genre: string;
-  createdAt: string;
-  updatedAt: string;
+    id: number;
+    title: string;
+    year: number;
+    genre: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_: any, { id }: MovieState) => ({ title: "hello", year: 2022 }),
-  },
-  Mutation: {
-    createMovie: (_: any, { title, year, genre }: MovieState) =>
-      client.movie.create({ data: { title, year, genre } }),
-    deleteMovie: (_: any, { title }: MovieState) => {
-      return true;
+    Query: {
+        movies: () => client.movie.findMany(),
+        movie: (_: any, { id }: MovieState) =>
+            client.movie.findUnique({ where: { id } }),
     },
-  },
+    Mutation: {
+        createMovie: (_: any, { title, year, genre }: MovieState) =>
+            client.movie.create({ data: { title, year, genre } }),
+        deleteMovie: (_: any, { id }: MovieState) =>
+            client.movie.delete({ where: { id } }),
+    },
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
 
 server
-  .listen(4000)
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+    .listen(4000)
+    .then(() => console.log("Server is running on http://localhost:4000/"));
