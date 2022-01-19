e048275f02a795276ad3240559142fb15882e473
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-19 12:52:15 +0900

    remove: 더미 데이터 삭제

--- src/Movies/movies.queries.ts
+++ src/Movies/movies.queries.ts
@@ -7,12 +7,4 @@ export default {
         movie: (_: any, { id }: Movie.Item) =>
             client.movie.findUnique({ where: { id } }),
     },
-    Mutation: {
-        createMovie: (_: any, { title, year, genre }: Movie.Item) =>
-            client.movie.create({ data: { title, year, genre } }),
-        updateMovie: (_: any, { id, year }: Movie.Item) =>
-            client.movie.update({ where: { id }, data: { year } }),
-        deleteMovie: (_: any, { id }: Movie.Item) =>
-            client.movie.delete({ where: { id } }),
-    },
 };
