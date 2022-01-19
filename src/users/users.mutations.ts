5b288a2ee66eca06e75e085824e724d13c9bb4e6
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-20 07:51:58 +0900

    feat: 계정 생성, 조회 완료

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import client from "@src/client";
 import { User } from "@src/types/user";
 
@@ -7,16 +8,33 @@ export default {
             _: any,
             { firstName, lastName, userName, email, password }: User.Item
         ) => {
-            //TODO 1: userName과 email이 DB에 존재하는지 확인하기
-            const isExistingUser = await client.user.findFirst({
-                where: { OR: [{ email }, { userName }] },
-            });
-            console.log(isExistingUser);
-            //TODO 2: 비밀번호 변조
-            //TODO 3: 유저 정보 저장
-            return client.user.create({
-                data: { firstName, lastName, userName, email, password },
-            });
+            try {
+                //TODO 1: userName과 email이 DB에 존재하는지 확인하기
+                const isExistingUser = await client.user.findFirst({
+                    where: { OR: [{ email }, { userName }] },
+                });
+
+                if (isExistingUser) {
+                    throw new Error(
+                        "이미 사용중인 닉네임 혹은 이메일 주소 입니다 "
+                    );
+                }
+                //TODO 2: 비밀번호 변조 (hash=> password, saltRound)
+                const modulatedPassword = await hash(password, 10);
+
+                //TODO 3: 유저 정보 저장
+                return client.user.create({
+                    data: {
+                        firstName,
+                        lastName,
+                        userName,
+                        email,
+                        password: modulatedPassword,
+                    },
+                });
+            } catch (e) {
+                return e;
+            }
         },
     },
 };
