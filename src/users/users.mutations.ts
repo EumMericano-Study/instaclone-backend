c0d0dd2e911d4836f5a73810411c7bc1daeef9b5
Eum Young Hyun <opwer032@naver.com>
2022-01-20 01:06:13 +0900

    docs: 해야할일, DB 저장시 주의사항 추가
    DB로 create 요청을 보낼 때
    unique값으로 설정한 것을 방어막으로 이용하지 말것
    
    DB의 오류를 마주하는 일은 절대 없도록 로직을 짜고,
    사전에 우회되는 경로가 없는지 주의할 것

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -6,9 +6,13 @@ export default {
     createAccount: (
       _: any,
       { firstName, lastName, userName, email, password }: User.Item
-    ) =>
-      client.user.create({
+    ) => {
+      //TODO 1: userName과 email이 DB에 존재하는지 확인하기
+      //TODO 2: 비밀번호 변조
+      //TODO 3: 유저 정보 저장
+      return client.user.create({
         data: { firstName, lastName, userName, email, password },
-      }),
+      });
+    },
   },
 };
