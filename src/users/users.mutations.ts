a996f9a6632bebe5434c757816b63038eb413e94
Eum Young Hyun <opwer032@naver.com>
2022-01-20 02:32:14 +0900

    feat: 회원가입 중복체크 확인

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -3,11 +3,15 @@ import { User } from "@src/types/user";
 
 export default {
   Mutation: {
-    createAccount: (
+    createAccount: async (
       _: any,
       { firstName, lastName, userName, email, password }: User.Item
     ) => {
       //TODO 1: userName과 email이 DB에 존재하는지 확인하기
+      const isExistingUser = await client.user.findFirst({
+        where: { OR: [{ email }, { userName }] },
+      });
+      console.log(isExistingUser);
       //TODO 2: 비밀번호 변조
       //TODO 3: 유저 정보 저장
       return client.user.create({
