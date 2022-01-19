d4de37e8a8eedeb28de28a33ee6b020f7a5a50dc
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-20 07:26:43 +0900

    feat: bcrypt 설치

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -2,21 +2,21 @@ import client from "@src/client";
 import { User } from "@src/types/user";
 
 export default {
-  Mutation: {
-    createAccount: async (
-      _: any,
-      { firstName, lastName, userName, email, password }: User.Item
-    ) => {
-      //TODO 1: userName과 email이 DB에 존재하는지 확인하기
-      const isExistingUser = await client.user.findFirst({
-        where: { OR: [{ email }, { userName }] },
-      });
-      console.log(isExistingUser);
-      //TODO 2: 비밀번호 변조
-      //TODO 3: 유저 정보 저장
-      return client.user.create({
-        data: { firstName, lastName, userName, email, password },
-      });
+    Mutation: {
+        createAccount: async (
+            _: any,
+            { firstName, lastName, userName, email, password }: User.Item
+        ) => {
+            //TODO 1: userName과 email이 DB에 존재하는지 확인하기
+            const isExistingUser = await client.user.findFirst({
+                where: { OR: [{ email }, { userName }] },
+            });
+            console.log(isExistingUser);
+            //TODO 2: 비밀번호 변조
+            //TODO 3: 유저 정보 저장
+            return client.user.create({
+                data: { firstName, lastName, userName, email, password },
+            });
+        },
     },
-  },
 };
