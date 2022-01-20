6d7f6f1fa3a4617e8e91bcb66e58ec532f9d60c5
Eum Young Hyun <opwer032@naver.com>
2022-01-21 00:02:35 +0900

    refactor: 유저 typeDef, resolvers 분할

---
+++ src/users/user.typeDefs.ts
@@ -0,0 +1,20 @@
+import { gql } from "apollo-server";
+
+/**
+ *  모델 제작 순서
+ * 1. prisma schema 정의
+ * 2. graphql schema 정의
+ * 3. mutations, queries 정의
+ */
+
+export default gql`
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String
+    userName: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
