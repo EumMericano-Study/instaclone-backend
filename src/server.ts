0ab67f2c8332c14a1bb6ff5d68fc1a1b3582af45
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-21 09:19:32 +0900

    feat: http 헤더를 이용해 토큰처리

--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,18 @@ import { ApolloServer } from "apollo-server";
 import schema from "@src/schema";
 
 /**
- * context를 이용해서 모든 스키마에서 사용할 수 있는 공동 변수를 지정할 수 있음
- * context는 스키마에서 3번째 argument로 제공된다.
+ * context를 이용해서 모든 resolver에서 사용할 수 있는
+ * Object 혹은 function들을 지정할 수 있음
+ * context는 resolver에서 3번째 argument로 제공된다.
+ *
+ * http 헤더를 통해 Authorization의 값으로 jwt token을 주고받는다.
  */
 const server = new ApolloServer({
     schema,
-    context: {
-        Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjQyNzIxMjg1fQ.8dYcvgIQjUQHTazSZGGZmUSUS7nNJxUcAocU-l48Rls",
+    context: ({ req }) => {
+        return {
+            Authorization: req.headers.authorization,
+        };
     },
 });
 
