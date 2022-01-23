39ef7055cc74dc4c1dc64c5d36b3c9c299ff196c
Eum Young Hyun <opwer032@naver.com>
2022-01-23 22:25:37 +0900

    learn: prisma 관계 데이터베이스 select 제한

--- src/users/seeProfile/seeProfile.resolvers.ts
+++ src/users/seeProfile/seeProfile.resolvers.ts
@@ -4,6 +4,16 @@ import { User } from "@src/types/user";
 export default {
   Query: {
     seeProfile: (_: any, { userName }: User.Item) =>
-      client.user.findUnique({ where: { userName } }),
+      client.user.findUnique({
+        where: { userName },
+        /**
+         * prisma의 include는 객체간의 관계를
+         * 한번에 모두 보여주고 싶을 때 사용한다.
+         *  include: {
+         *    followers: true,
+         *    following: true,
+         *  },
+         */
+      }),
   },
 };
--- src/users/user.typeDefs.ts
+++ src/users/user.typeDefs.ts
@@ -7,6 +7,19 @@ import { gql } from "apollo-server";
  * 3. mutations, queries 정의
  */
 
+/**
+ * gql에서 following, followers같이
+ * 다른 테이블과 연관된 항목들은
+ * db에 의도치 않은 과부화를 불러올 수 있으므로
+ * prisma에서 기본적으로 막고 있다.
+ * 이를 허용하고자 한다면
+ *
+ * prisma resolver설정에서 include를 설정을 하여야
+ * 한번에 연관된 테이블의 내용까지 모두 보여줄 수 있다. (비추)
+ * db에 부담이 될 relationship이라면
+ * 이를 pagenation기능으로 구현하는 것이 추천된다.
+ */
+
 export default gql`
   type User {
     id: Int!
@@ -18,6 +31,9 @@ export default gql`
     avatar: String
     createdAt: String!
     updatedAt: String!
+
+    following: [User]
+    followers: [User]
   }
 
   scalar Upload
