6c13c8840be43c8aaaf4f96e88913e43d19fa846
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-25 08:16:07 +0900

    feat: User Computed field 추가

--- src/users/user.resolvers.ts
+++ src/users/user.resolvers.ts
@@ -1,5 +1,72 @@
+import client from "@src/client";
 import { GraphQLUpload } from "graphql-upload";
+import { User } from "@src/types";
+/**
+ * DB에는 존재하지 않지만 type에는 정의한
+ * ComputedFields 항목의 resolver를
+ * 내마음대로 설정할 수 있다.
+ *
+ * 호출 과정
+ * Mutation 호출 시 totalFollowing값 호출 => Mutation을 통해 totalFollowing 값을 탐색
+ * => 존재하지 않는다면 type resolver에서 탐색 => 없다면 에러, 있다면 출력
+ *
+ * root값은 mutation에서 호출된 객체의 정보를 담고 있다
+ */
 
+interface Root {
+    id: number;
+}
+interface Context {
+    loggedInUser: User.Item;
+}
+/**
+ * 실제 인스타그램은 이런식으로 카운팅 하지 않음,
+ * 너무 많은 데이터를 내포하다보니 실시간 동기화가 거의 불가능함
+ * (DB를 여러개로 사용하기 때문)
+ * 실제 인스타에서는 Eventual consistency라는 것을 사용
+ *
+ * ex) 호날두의 진짜 팔로워수는 나라마다 조금씩 상이할 수 있음.
+ */
 export default {
-  Upload: GraphQLUpload,
+    // count 하기 위해 역으로 숫자를 셈
+    // graphql에서 자동으로 await동작을 함
+    User: {
+        totalFollowing: ({ id }: Root) =>
+            client.user.count({
+                where: {
+                    followers: {
+                        some: { id },
+                    },
+                },
+            }),
+        totalFollowers: ({ id }: Root) =>
+            client.user.count({
+                where: {
+                    following: {
+                        some: { id },
+                    },
+                },
+            }),
+        isMe: ({ id }: Root, _: any, { loggedInUser }: Context) => {
+            if (!loggedInUser) return false;
+            return id === loggedInUser.id;
+        },
+        isFollowing: async (
+            { id }: Root,
+            _: any,
+            { loggedInUser }: Context
+        ) => {
+            if (!loggedInUser) return false;
+            //존재하면 1, 없으면 0
+            const exists = await client.user.count({
+                where: {
+                    userName: loggedInUser.userName,
+                    following: { some: { id } },
+                },
+            });
+            return Boolean(exists);
+        },
+    },
+
+    Upload: GraphQLUpload,
 };
--- src/users/user.typeDefs.ts
+++ src/users/user.typeDefs.ts
@@ -17,24 +17,41 @@ import { gql } from "apollo-server";
  * prisma resolver설정에서 include를 설정을 하여야
  * 한번에 연관된 테이블의 내용까지 모두 보여줄 수 있다. (비추)
  * db에 부담이 될 relationship이라면
- * 이를 pagenation기능으로 구현하는 것이 추천된다.
+ * 이를 pageNation기능으로 구현하는 것이 추천된다.
+ */
+
+/**
+ * gql에서는 Computed Fields라는 개념을 사용할 수 있다.
+ * 서비스 페이지에서
+ * 해당 페이지가 사용자마다 다르게 보일 값들을
+ * 마치 DB에 저장되어있는것 처럼 뽑아올 수 있다.
+ *
+ * ex ) 인스타그램 내 페이지, 팔로우 한 페이지, 팔로우 하지 않은 페이지
+ *     모두 다른 UI를 보여주고 (버튼이 바뀜)
+ *     이를 구분할 수 있는 데이터를
+ *     isMe, isFollowing과 같이 계산해서 한번에 보내줄 수 있음
  */
 
 export default gql`
-  type User {
-    id: Int!
-    firstName: String!
-    lastName: String
-    userName: String!
-    email: String!
-    bio: String
-    avatar: String
-    createdAt: String!
-    updatedAt: String!
+    type User {
+        id: Int!
+        firstName: String!
+        lastName: String
+        userName: String!
+        email: String!
+        bio: String
+        avatar: String
+        createdAt: String!
+        updatedAt: String!
 
-    following: [User]
-    followers: [User]
-  }
+        following: [User]
+        followers: [User]
 
-  scalar Upload
+        totalFollowing: Int!
+        totalFollowers: Int!
+        isMe: Boolean!
+        isFollowing: Boolean!
+    }
+
+    scalar Upload
 `;
