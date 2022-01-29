c3eada973687f282403b0b26abd495395769a6a4
Eum Young Hyun <opwer032@naver.com>
2022-01-29 19:01:31 +0900

    feat: 인덱스 타입 설명 추가

--- src/types/resolvers.d.ts
+++ src/types/resolvers.d.ts
@@ -1,16 +1,21 @@
 import { Context } from "./context";
 
 export type Resolver = (
-    root: any,
-    args: any,
-    context: Context,
-    info: any
+  root: any,
+  args: any,
+  context: Context,
+  info: any
 ) => any;
 
 type Resolvers = {
-    [key: string]: {
-        [key: string]: Resolver;
-    };
+  /**
+   * 인덱스 타입
+   * 인터페이스에서 속성 이름을 구체적으로 정의하지 않고
+   * 값의 타입만 정의하는 것을 인덱스(index) 타입이라고 함
+   */
+  [key: string]: {
+    [key: string]: Resolver;
+  };
 };
 
 export default Resolvers;
