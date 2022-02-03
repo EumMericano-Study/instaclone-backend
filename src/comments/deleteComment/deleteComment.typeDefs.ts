e40bd1068b5844a93d8ade0e372fb0b263faa503
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-02-03 09:20:26 +0900

    refactor: MutationResponse 획일화

--- src/comments/deleteComment/deleteComment.typeDefs.ts
+++ src/comments/deleteComment/deleteComment.typeDefs.ts
@@ -1,11 +1,7 @@
 import { gql } from "apollo-server";
 
 export default gql`
-  type DeleteCommentResult {
-    ok: Boolean!
-    error: String
-  }
-  type Mutation {
-    deleteComment(id: Int!): DeleteCommentResult!
-  }
+    type Mutation {
+        deleteComment(id: Int!): MutationResponse!
+    }
 `;
